2d249eea1658e1d8747fe94402479a3a3832cda3
rknmovies <raajkiccha97@gmail.com>
2026-01-31 12:51:11 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -22,7 +22,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
-                        [InlineKeyboardButton("😊 ᴍᴏᴠɪᴇ ᴄʜᴀɴɴᴇʟ", url="https://t.me/+r_bKvfgow4wxZDY1")],
+                        [InlineKeyboardButton("😊 ᴍᴏᴠɪᴇ ᴄʜᴀɴɴᴇʟ", url="https://t.me/+thggatXBL2xlZTk9")],
                     
     [
                         InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
@@ -38,7 +38,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
-                        [InlineKeyboardButton("😊 ᴍᴏᴠɪᴇ ᴄʜᴀɴɴᴇʟ", url="https://t.me/+r_bKvfgow4wxZDY1")],
+                        [InlineKeyboardButton("😊 ᴍᴏᴠɪᴇ ᴄʜᴀɴɴᴇʟ", url="https://t.me/+thggatXBL2xlZTk9")],
                     
     [
                         InlineKeyboardButton('ʜᴏᴍᴇ', callback_data='start'),
